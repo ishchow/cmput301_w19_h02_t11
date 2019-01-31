be8b7eeb052845c5024720a9e9fc12bac8433a38
Ishaat Chowdhury <ishaat@ualberta.ca>
2019-01-30 18:34:51 -0700

    Add color

--- app/src/main/java/com/example/cmput301lab4demo/Shape.java
+++ app/src/main/java/com/example/cmput301lab4demo/Shape.java
@@ -3,4 +3,35 @@ package com.example.cmput301lab4demo;
 public abstract class Shape {
     private int x;
     private int y;
+    private String color;
+
+    public Shape(int x, int y, String color) {
+        this.x = x;
+        this.y = y;
+        this.color = color;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
 }
