9d58223eb97b0f5c32c738da2b50cf1f310d66ed
Alex Chen <yin6@ualberta.ca>
2019-01-30 17:38:31 -0700

    Added my shape

--- app/src/main/java/com/example/cmput301lab4demo/Shape.java
+++ app/src/main/java/com/example/cmput301lab4demo/Shape.java
@@ -3,20 +3,4 @@ package com.example.cmput301lab4demo;
 public abstract class Shape {
     private int x;
     private int y;
-
-    public int getX() {
-        return x;
-    }
-
-    public void setX(int x) {
-        this.x = x;
-    }
-
-    public int getY() {
-        return y;
-    }
-
-    public void setY(int y) {
-        this.y = y;
-    }
 }
