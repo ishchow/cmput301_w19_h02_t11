753207b7db3949e51e875007faeed0f37aad24ce
Alex Chen <yin6@ualberta.ca>
2019-01-30 17:24:44 -0700

    Added getters and setters to x, y. This is a test commit

--- app/src/main/java/com/example/cmput301lab4demo/Shape.java
+++ app/src/main/java/com/example/cmput301lab4demo/Shape.java
@@ -2,5 +2,22 @@ package com.example.cmput301lab4demo;
 
 public abstract class Shape {
     private int x;
+
+    public int getX() {
+        return x;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
     private int y;
 }
