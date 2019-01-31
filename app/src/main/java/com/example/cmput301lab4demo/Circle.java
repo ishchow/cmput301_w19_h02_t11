840573b092b22a4a566d0f06e9440a0612a2b801
siyuamymi <siyuamymi@gmail.com>
2019-01-30 18:39:43 -0700

    I'm lost

--- app/src/main/java/com/example/cmput301lab4demo/Circle.java
+++ app/src/main/java/com/example/cmput301lab4demo/Circle.java
@@ -2,7 +2,7 @@ package com.example.cmput301lab4demo;
 
 public class Circle extends Shape{
 
-    public Circle(){
-        super();
+    public Circle(int x, int y, int z, String color){
+        super(x, y, z, color);
     }
 }
